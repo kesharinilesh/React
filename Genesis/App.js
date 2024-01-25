c2d5f3ef8b05b2e90a0484ea046e1c898064a2fc
Nilesh Keshari <kesharinilesh13@gmail.com>
2024-01-25 16:10:14 +0530

    Basics of React

---
+++ Genesis/App.js
@@ -0,0 +1,30 @@
+const heading = React.createElement("h1",{id:"heading"},"Hello World from React!");
+console.log(heading); // React elements are just objects
+
+// React Element(Object)==> HTML(Browser Understands)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);   // While rendering the objects are converted into html elements and puts up into the DOM
+
+
+{/* 
+<div id="parent">
+    <div id="child1">
+        <h1>I'm a h1 tag</h1>
+        <h2>I'm a h2 tag</h2>
+    </div>
+    <div id="child2">
+        <h1>I'm a h1 tag</h1>
+        <h2>I'm a h2 tag</h2>
+    </div>
+</div>  
+*/}  
+
+// How to create this type of structure using react?
+
+const parent=React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child"},[React.createElement("h1",{},"I am a h1 tag"),React.createElement("h1",{},"I am a h2 tag")]),React.createElement("div",{id:"child"},[React.createElement("h1",{},"I am a h1 tag"),React.createElement("h1",{},"I am a h2 tag")])]);
+const root2=ReactDOM.createRoot(document.getElementById("root2"));
+
+// this code looks a bit tidy that's why we use jsx to write react.
+root2.render(parent);
