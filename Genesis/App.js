a50b68f1a36350acf272d91ff5b38e4b8fc3f443
Nilesh Keshari <kesharinilesh13@gmail.com>
2024-02-01 00:23:19 +0530

    starters finished

--- Genesis/App.js
+++ Genesis/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const heading = React.createElement("h1",{id:"heading"},"Hello World from React!");
 console.log(heading); // React elements are just objects
 
@@ -23,7 +26,13 @@ root.render(heading);   // While rendering the objects are converted into html e
 
 // How to create this type of structure using react?
 
-const parent=React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child"},[React.createElement("h1",{},"I am a h1 tag"),React.createElement("h1",{},"I am a h2 tag")]),React.createElement("div",{id:"child"},[React.createElement("h1",{},"I am a h1 tag"),React.createElement("h1",{},"I am a h2 tag")])]);
+const parent=React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child"},
+[React.createElement("h1",{},"I am a h1 tag"),
+React.createElement("h1",{},"I am a h2 tag")]),
+React.createElement("div",{id:"child"},
+[React.createElement("h1",{},"I am a h1 tag"),
+React.createElement("h1",{},"I am a h2 tag")])]);
+
 const root2=ReactDOM.createRoot(document.getElementById("root2"));
 
 // this code looks a bit tidy that's why we use jsx to write react.
