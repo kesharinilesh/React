a192588b05312d7e39aac2bbfac8d9fb23f0dc1e
Nilesh Keshari <kesharinilesh13@gmail.com>
2024-03-11 23:39:19 +0530

    used props

--- Inception/App.js
+++ Inception/App.js
@@ -21,12 +21,16 @@ const Header = () => {
 const styleCard={
     backgroundColor: "blue",
 }
-const RestaurantCard = ()=>{
+const RestaurantCard = (props)=>{
+    console.log(props);
+    const {resName, cuisine, stars, time}=props
     return (
         <div className="res-card" style={{backgroundColor:"#F0F0F0"}}>  
         {/* another way to write css. */}
         <img className="res-logo" alt="logo" src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/535fc9f9c135f7982317bbb6a64a1ffc'/>
-            <h3> McDonald's</h3>
+            <h3> {props.resName}</h3>
+            <h4> {props.cuisine}</h4>
+            <h4> {props.stars} | {props.time}</h4>
         </div>
         // <div className="res-card" style={styleCard}>  
         // {/* Another way to write css. */}
@@ -42,7 +46,14 @@ const Body=()=>{
             <button className='search-button'>Search</button>
         </div>
         <div className='restaurant-container'>
+            <RestaurantCard resName="McDonald's" cuisine="American" stars="4.3⭐" time="25-30 mins"/>
+            <RestaurantCard resName="KFC" cuisine="Non-Veg" stars="4.1⭐" time="25-30 mins"/>
+            {/* <RestaurantCard/>
+            <RestaurantCard/>  How to create these dynamically for others? by using props like passing props to a component (which means. passing a prop to a function is like passing an argument to a function)(properties)
+
             <RestaurantCard/>
+            <RestaurantCard/>
+            <RestaurantCard/> */}
         </div>
     </div>
     )
