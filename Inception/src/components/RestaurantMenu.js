68bd4ae97eae5865cdeb3aeffed686c30d2d6b25
Nilesh Keshari <kesharinilesh13@gmail.com>
2024-04-18 09:27:23 +0530

    fixed price bug

--- Inception/src/components/RestaurantMenu.js
+++ Inception/src/components/RestaurantMenu.js
@@ -30,7 +30,7 @@ const RestaurantMenu = () => {
             <p>{cuisines.join(", ")} - {costForTwoMessage}</p>
             <h3>Menu</h3>
             <ul>
-                {itemCards.map(item=><li key={item.card.info.id}>{item.card.info.name} - Rs.{item.card.info.defaultPrice/100}</li>)}
+                {itemCards.map(item=><li key={item.card.info.id}>{item.card.info.name} - Rs.{item.card.info.defaultPrice/100 ||item.card.info.price/100}</li>)}
             </ul>
         </div>
     )
