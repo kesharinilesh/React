fcb87e5e2673d0cf856ad87d3312b9d700f0bd2e
Nilesh Keshari <kesharinilesh13@gmail.com>
2024-04-20 00:01:07 +0530

    single page principle

--- Inception/src/components/RestaurantMenu.js
+++ Inception/src/components/RestaurantMenu.js
@@ -1,22 +1,13 @@
 import { useState,useEffect } from "react";
 import Shimmer from "./Shimmer";
 import {useParams} from "react-router-dom";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-    const [resInfo,setResInfo] = useState(null);
     const {resId} = useParams();
-    console.log(resId);
 
-    useEffect (()=>{ 
-            fetchMenu();
-        },[]);
-    const fetchMenu = async () => {
-        const data = await fetch('https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.4969&lng=80.3246&restaurantId='+resId+'&isMenuUx4=true&submitAction=ENTER')
-        const json = await data.json();
-        console.log(json);
-        setResInfo(json.data);
-    };
-
+    const resInfo = useRestaurantMenu(resId);
+    console.log(resInfo);
     
     if (resInfo === null) 
         return <Shimmer />;
